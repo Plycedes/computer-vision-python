8fa2bc9fea7a0c3cb5f971c6bbf40049778109c4
Abhay Pratap Yadav <98642540+Plycedes@users.noreply.github.com>
2023-01-09 00:28:47 +0530

    changes

--- BasicCompVis/readingFile.py
+++ BasicCompVis/readingFile.py
@@ -1,9 +1,9 @@
 import cv2
 
-image = cv2.imread('img.png', 1)
+image = cv2.imread('img.png', cv2.IMREAD_COLOR)
 
 window_name = 'theimage'
 
-cv2.imshow(window_name, image)
+cv2.imshow("Abhay", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
